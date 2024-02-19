dbd5655fba7914f2bd36fd3ad352f35a2fef4d43
Ritik220519 <ritik220519@gmail.com>
2024-02-20 00:03:49 +0530

    add structure changes

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import dotenv from "dotenv";
+import connectDB from "./db/indexDbConnection.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ;( async()=>{
+// try {
+//    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//    app.on("error" , (err)=>{
+//     console.log("Error" , err);
+//    })
+//    app.listen(process.env.port, ()=>{
+//     console.log(`App is listing on port ${process.env.port}`)
+//    })
+// } catch (error) {  
+//         console.log("Error" , error);    
+// }
+// })()
